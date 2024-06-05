7d0c4f05a3f4080d45e4a5954a326276f133ffc0
Khairi SLAMA <khairi.slama.1996.br@gmail.com>
2024-06-05 17:38:05 +0100

    feat: :sparkles: init new vit project with a three basic sphere with loop rendering

---
+++ main.js
@@ -0,0 +1,57 @@
+import * as THREE from "three";
+import "./style.css";
+
+//Scene
+const scene = new THREE.Scene();
+
+// Create the sphere
+const geometry = new THREE.SphereGeometry(3, 64, 64);
+const material = new THREE.MeshStandardMaterial({
+  color: "#00ff83",
+});
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+// Sizes
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
+// Light
+const light = new THREE.PointLight(0xffffff, 100, 100);
+light.position.set(0, 10, 10);
+scene.add(light);
+
+// Camera
+const camera = new THREE.PerspectiveCamera(
+  45,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
+camera.position.z = 20;
+scene.add(camera);
+
+// Renderer
+const canvas = document.querySelector(".webgl");
+const renderer = new THREE.WebGLRenderer({ canvas });
+renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene, camera);
+
+// Resize
+window.addEventListener("resize", () => {
+  // Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+  // Update camera
+  camera.updateProjectionMatrix();
+  camera.aspect = sizes.width / sizes.height;
+  renderer.setSize(sizes.width, sizes.height);
+});
+
+const loop = () => {
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(loop);
+};
+loop();
